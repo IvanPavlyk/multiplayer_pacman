9311599a33c39e18ab33d973991b3039dd4f135f
AdrianWTWong <adrianwong@live.ca>
2021-07-17 22:28:12 -0700

    Add database connection functionality
    - Add post functionality to database for new users using uuid
    - Add environment variable parsing
    - Add react request sending
    
    *Pair programming w/ Steven to deploy Google authentication

--- pac-svc/src/app.ts
+++ pac-svc/src/app.ts
@@ -1,14 +1,45 @@
 import { Server } from "colyseus";
 import { createServer } from "http";
+import { v4 as uuidv4 } from "uuid";
+import * as dotenv from "dotenv";
+import cors from 'cors';
 import express from "express";
-const port = Number(process.env.port) || 3000;
+import * as pg from "pg";
 
+const port = Number(process.env.port) || 3001;
 const app = express();
+dotenv.config()
+
+app.set("port", 3002);
+app.use(cors());
 app.use(express.json());
+
+const { Pool } = pg;
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+app.post("/add-user", async (req, res) => {
+    const { name, email } = req.body;
+    const template = `INSERT INTO pacman."User"(id, username, email) VALUES ($1, $2, $3)`;
+    
+    let response;
+    try {
+        response = await pool.query(template, [uuidv4(), name, email]);
+    } catch (error){
+        response = error;
+    }
+
+    res.send(response);
+});
+
+app.listen(app.get("port"), () => {
+    console.log(`Server at: http://localhost:${app.get("port")}/`);
+});
 
 // https://docs.colyseus.io/server/api/#new-server-options
 const gameServer = new Server({
-  server: createServer(app)
+    server: createServer(app),
 });
 
 gameServer.listen(port);
--- pac-web/src/pages/Login/Login.js
+++ pac-web/src/pages/Login/Login.js
@@ -1,41 +1,41 @@
 import React from "react";
-import { Form, Button, Container, Row } from "react-bootstrap";
+import axios from 'axios';
+import { Container, Row } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { GoogleLogin } from "react-google-login";
 import "./style.css";
 
-
 const Login = () => {
     const history = useHistory();
 
-    const handleLogin = (response) => {
-        console.log(response);
-        history.push("/room"); //TODO: push to create account page
+    const handleLoginSuccess = (response) => {
+        const profile = response.profileObj;
+        const user = {
+            name: profile.name,
+            email: profile.email
+        }
+
+        axios.post("http://localhost:3002/add-user", user)
+        .then( res => {
+            history.push("/room"); //TODO: push to create account page
+        });
+
+    };
+
+    const handleLoginFailure = (response) => {
+        console.error(response);
     };
 
     return (
         <Container>
             <Row className="justify-content-center">
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
                 <GoogleLogin
                     clientId={
                         "1082753993159-va32d2tcalpqv67hnc0apngd0hsk48e0.apps.googleusercontent.com"
                     }
                     buttonText="Log in with Google"
-                    onSuccess={handleLogin}
-                    onFailure={handleLogin}
+                    onSuccess={handleLoginSuccess} //might only use one function here
+                    onFailure={handleLoginFailure}
                     cookiePolicy={"single_host_origin"}
                 />
             </Row>
