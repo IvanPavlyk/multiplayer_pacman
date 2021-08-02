217031d637c3580982a75b2afba98540e7debfb4
Ivan Pavlyk <ipa11@sfu.ca>
2021-08-01 17:08:28 -0700

    Implemented grid locking with Ivan V.

--- pac-svc/src/rooms/GameRoom.ts
+++ pac-svc/src/rooms/GameRoom.ts
@@ -15,28 +15,76 @@ class GameRoom extends Room<GameState> {
 		});
 
 		this.onMessage('initMap', (client, message) => {
+			this.state.counter += 1;
 			if(!this.state.pellets.length){
-			// console.log("YO RECEIVED");
-			//this.state.pellets = message.pellets;
 				message.pellets.forEach(ele =>{
 					this.state.pellets.push(ele);
 				});
 			}
+			if(!this.state.walls.length){
+				message.walls.forEach(ele =>{
+					this.state.walls.push(ele);
+				});
+			}
+			if(!this.state.width){
+				this.state.width = message.width;
+			}
+			if(!this.state.height){
+				this.state.height = message.height;
+			}
+		
 		});
 
 		this.onMessage('moving', (client, message) => {
 			const player = this.state.players.get(client.id);
 			player.direction = message?.direction;
+			player.queuedDirection = message?.queuedDirection;
+			console.log({...player});
+		});
+
+		this.onMessage('checkQueue', (client, message) => {
+			const player = this.state.players.get(client.id);
 			player.x = message?.x;
 			player.y = message?.y;
+			if(player.queuedDirection !== player.direction){
+				if(player.queuedDirection === 'up' && !this.state.walls[Math.floor(player.x / 32) + Math.floor((player.y/ 32) - 1) * this.state.width]){
+					player.direction = 'up';
+					player.x = Math.floor(player.x/32)*32 + 16;//+ Number(player.direction === 'left')*-15 + Number(player.direction === 'right')*15;
+				}
+				if(player.queuedDirection === 'down' && !this.state.walls[Math.floor(player.x / 32) + Math.floor((player.y/ 32) + 1) * this.state.width]){
+					player.direction = 'down';
+					player.x = Math.floor(player.x/32)*32 + 16;//+ Number(player.direction === 'left')*-15 + Number(player.direction === 'right')*15;
+				}
+				if(player.queuedDirection === 'left' && !this.state.walls[Math.floor(player.x / 32 - 1) + Math.floor(player.y / 32) * this.state.width]){
+					player.direction = 'left';
+					player.y = Math.floor(player.y/32)*32 + 16;//+ Number(player.direction === 'down')*-15 + Number(player.direction === 'up')*15;
+					console.log("y ==================== ", player.y);
+				}
+				if(player.queuedDirection === 'right' && !this.state.walls[Math.floor(player.x / 32 + 1) + Math.floor(player.y / 32) * this.state.width]){
+					player.direction = 'right';
+					player.y = Math.floor(player.y/32)*32 + 16;//+ Number(player.direction === 'down')*-15 + Number(player.direction === 'up')*15;
+				}
+			}
+			
 		});
+		this.onMessage('gridLock', (client, message) => {
+			const player = this.state.players.get(client.id);
+			if(['right', 'left'].indexOf(message?.direction) !== -1){
+				player.y = Math.floor(message?.y/32)*32 + 16;
+			}
+			if(['up', 'down'].indexOf(message?.direction) !== -1){
+				player.x = Math.floor(message?.x/32)*32 + 16;
+			}
+
+		});
+
 		this.onMessage('pelletEaten', (client, message) => {
 			this.state.pellets[message.pellet] = 0;
 		});
 	}
 	
 	onJoin(client: Client) {
-		this.state.players.set(client.id, new Player());
+		this.state.players.set(client.id, new Player({x: 32*5 + 16, y: 32*10 + 16}));
 	}
 	
 	async onLeave(client: Client) {
--- pac-svc/src/schema/GameState.ts
+++ pac-svc/src/schema/GameState.ts
@@ -4,6 +4,10 @@ import Player from './entities/Player';
 class GameState extends Schema {
 	@type({ map: Player }) players = new MapSchema<Player>();
 	@type( ["number"] ) pellets = new ArraySchema<number>();
+	@type( ["number"] ) walls = new ArraySchema<number>();
+	@type( "number" ) width = undefined;
+	@type( "number" ) height = undefined;
+	@type( "number" ) counter = 0;
 	@type('boolean') gameStarted = false;
 
 	@type(Player) admin
