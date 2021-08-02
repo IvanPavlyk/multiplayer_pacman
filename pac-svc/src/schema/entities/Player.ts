59c00b00111c7c6f4e4e5a62372db7f1c86ab8d9
Ivan Pavlyk <ipa11@sfu.ca>
2021-08-01 17:08:28 -0700

    Implemented grid locking with Ivan V.

--- pac-svc/src/schema/entities/Player.ts
+++ pac-svc/src/schema/entities/Player.ts
@@ -3,11 +3,14 @@ import { type, Schema } from "@colyseus/schema";
 class Player extends Schema {
 	@type('boolean') ready = false;
 	@type('string') direction =  'right';
-	@type('number') x =  Math.floor(Math.random() * 300) + 50;
-	@type('number') y = Math.floor(Math.random() * 300) + 50;
+	@type('string') queuedDirection =  'right';
+	@type('number') x =  0;
+	@type('number') y = 0;
 
-  constructor() {
+  constructor({x, y}) {
     super();
+	this.x = x;
+	this.y = y;
   }
 }
 
