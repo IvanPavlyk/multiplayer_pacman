bde0c0fb8f9b807e6538a6fa769b48b37872abb9
AdrianWTWong <adrianwong@live.ca>
2021-08-04 19:22:17 -0700

    Add username creation
    - Add page for username addition to creating a new user

--- pac-web/src/pages/NewAccount/NewAccount.js
+++ pac-web/src/pages/NewAccount/NewAccount.js
@@ -1,10 +1,17 @@
 import React, {useState} from 'react';
 import axios from 'axios';
 import {Form, Button, Container} from 'react-bootstrap';
+import { useLocation, withRouter, useHistory } from 'react-router';
 
 
-const NewAccount = (googleUserProfile) => {
-  const [accountInfo, setAccountInfo] = useState(googleUserProfile);
+const NewAccount = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const [accountInfo, setAccountInfo] = useState(
+    location.state.detail
+  );
+
+  console.log(accountInfo);
 
   const handleChange = (event) => {
     setAccountInfo(
@@ -14,11 +21,12 @@ const NewAccount = (googleUserProfile) => {
     );
   };
   
-  const newAccountHandler = () => {
+  const newAccountHandler = (event) => {
+    event.preventDefault();
     axios.post('http://localhost:3002/add-user', accountInfo) //TODO: push url to enviornment variable or helper function
       .then( res => {
         console.log(res);
-        history.push('/home'); //TODO: push to create account page
+        history.push('/home');
       })
       .catch ( error => {
         console.error(error);
@@ -27,15 +35,15 @@ const NewAccount = (googleUserProfile) => {
 
   return (
     <Container>
-      <Form>
+      <Form onSubmit={newAccountHandler}>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>PLAYER NAME</Form.Label>
           <Form.Control type='text' onChange={handleChange}/>
         </Form.Group>
-        <Button variant='primary' type='submit' onClick={newAccountHandler}>Submit</Button>
+        <Button variant='primary' type='submit'>Submit</Button>
       </Form>
     </Container>
   );
 };
 
-export default NewAccount;
+export default withRouter(NewAccount);
