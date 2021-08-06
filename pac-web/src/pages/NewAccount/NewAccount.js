b920ecf50df0110be6b32720b6ce8996bb612b45
ivuckovi <ivan_vuckovic@sfu.ca>
2021-08-05 22:40:42 -0700

    bug fix and lint

--- pac-web/src/pages/NewAccount/NewAccount.js
+++ pac-web/src/pages/NewAccount/NewAccount.js
@@ -1,33 +1,27 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import {Form, Button, Container} from 'react-bootstrap';
+import { Form, Button, Container } from 'react-bootstrap';
 import { useLocation, withRouter, useHistory } from 'react-router';
-
 
 const NewAccount = () => {
   const location = useLocation();
   const history = useHistory();
-  const [accountInfo, setAccountInfo] = useState(
-    location.state.detail
-  );
+  const [accountInfo, setAccountInfo] = useState(location.state.detail);
 
   const handleChange = (event) => {
-    setAccountInfo(
-      {...accountInfo,
-        username: event.target.value
-      }
-    );
+    setAccountInfo({ ...accountInfo, username: event.target.value });
   };
-  
+
   const newAccountHandler = (event) => {
     event.preventDefault();
-    axios.post('http://localhost:3002/add-user', accountInfo) //TODO: push url to enviornment variable or helper function
-      .then( res => {
-        console.log(res);
-        sessionStorage.setItem('id',res.data);
+    axios
+      .post('http://localhost:3002/add-user', accountInfo) //TODO: push url to enviornment variable or helper function
+      .then((res) => {
+        sessionStorage.setItem('id', res.data);
+        sessionStorage.setItem('userName', accountInfo.username);
         history.push('/home');
       })
-      .catch ( error => {
+      .catch((error) => {
         console.error(error);
       });
   };
@@ -36,9 +30,11 @@ const NewAccount = () => {
     <Container>
       <Form onSubmit={newAccountHandler}>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
-          <Form.Control type='text' onChange={handleChange} placeholder='USERNAME'/>
+          <Form.Control type='text' onChange={handleChange} placeholder='USERNAME' />
         </Form.Group>
-        <Button variant='primary' type='submit'>Submit</Button>
+        <Button variant='primary' type='submit'>
+          Submit
+        </Button>
       </Form>
     </Container>
   );
